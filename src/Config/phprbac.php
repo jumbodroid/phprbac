785e375b794582f3ffa222b87078c630094bb2bb
Alois Odhiambo <rayalois22@gmail.com>
2020-08-18 17:42:52 +0300

    Bug fix

--- src/Config/phprbac.php
+++ src/Config/phprbac.php
@@ -7,10 +7,10 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'host'          => "localhost",
-    'user'          => "root",
-    'pass'          => "",
-    'dbname'        => "phprbac",
+    'host'          => env('DB_HOST', '127.0.0.1'),
+    'user'          => env('DB_USERNAME', 'forge'),
+    'pass'          => env('DB_PASSWORD', ''),
+    'dbname'        => env('DB_DATABASE', 'forge'),
     'adapter'       => "pdo_mysql",
     'tablePrefix'   => "rbac_",
 ];
